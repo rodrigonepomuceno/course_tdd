76aefd31e09ba81fc5b6304327e12d08d1bcba4c
RodrigoN <rodrigonepomuceno.rn@gmail.com>
2020-11-23 19:45:43 -0300

    chage authentication usecase receive a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,8 +1,14 @@
 import 'package:meta/meta.dart';
-
+import 'package:meta/meta.dart';
 import '../entities/entities.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth(
-      {@required String email, @required String password}) async {}
+  Future<AccountEntity> auth(AuthenticationParams params) async {}
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({this.email, this.secret});
 }
