e8c7b618b63f0b8e996c4595a8d36791b1ca350f
RodrigoN <rodrigonepomuceno.rn@gmail.com>
2020-11-25 19:14:05 -0300

    final aula 13

---
+++ lib/main/main.dart
@@ -0,0 +1,6 @@
+import '../ui/components/components.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
---
+++ lib/ui/pages/login_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Image(
+                image: AssetImage('lib/ui/assets/logo.png'),
+              ),
+            ),
+            Text('LOGIN'),
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'E-mail', icon: Icon(Icons.email)),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Senha', icon: Icon(Icons.lock)),
+                    obscureText: true,
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text('ENTRAR'),
+                  ),
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.person),
+                    label: Text('Criar Conta'),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
