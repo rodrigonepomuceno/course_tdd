275b22ec10a2c3e537e453ea020dee731df3fcf9
RodrigoN <rodrigonepomuceno.rn@gmail.com>
2020-12-02 17:58:07 -0300

    final aula 21

---
+++ test/presentation/presenters/stream_login_presenters_test.dart
@@ -0,0 +1,41 @@
+import 'package:course_tdd/presentation/presenters/presenters.dart';
+import 'package:course_tdd/presentation/protocols/protocols.dart';
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class ValidationSpy extends Mock implements Validation {}
+
+void main() {
+  StreamLoginPresenter sut;
+  ValidationSpy validation;
+  String email;
+
+  PostExpectation mockValidationCall(String field) => when(
+        validation.validate(field: field == null ? anyNamed('field') : field, value: anyNamed('value')),
+      );
+
+  void mockValidation({String field, String value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
+
+  setUp(() {
+    validation = ValidationSpy();
+    sut = StreamLoginPresenter(validation: validation);
+    email = faker.internet.email();
+    mockValidation();
+  });
+  test('Should call validation with corret email', () {
+    sut.validateEmail(email);
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit email error if validation fails', () {
+    mockValidation(value: 'error');
+
+    //espera um retorno futuro
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
+  });
+}
